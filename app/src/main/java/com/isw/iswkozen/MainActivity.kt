22f5c64fa698903b29e2286dbb19a8df52d1a06d
Ayooluwa olosunde <ayooluwa.olosunde@gmail.com>
2022-10-11 09:07:08 +0100

    feature:iso

--- app/src/main/java/com/isw/iswkozen/MainActivity.kt
+++ app/src/main/java/com/isw/iswkozen/MainActivity.kt
@@ -7,7 +7,6 @@ import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
